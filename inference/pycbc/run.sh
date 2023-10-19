d0c7133e6ffcb4de943d5461e2bc44fe308e6cd4
Komal Thareja <kthare10@renci.org>
2023-10-18 22:12:34 -0400

    update run script to download data

--- inference/pycbc/run.sh
+++ inference/pycbc/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+cd /home/poseidon/workflows/pycbc/inference/pycbc/data/
+./get.sh
+
 cd /home/poseidon/workflows/pycbc/inference/pycbc/gw_output/
 pegasus-plan --conf ./pegasus-properties.conf --dir submitdir --output-sites local --sites condorpool_copy,local --cluster label,horizontal --cleanup inplace --relative-dir work -q --submit --dax gw.dax
 sleep 60
