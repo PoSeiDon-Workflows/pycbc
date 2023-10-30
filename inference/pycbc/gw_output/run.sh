860499b6e134e17f24ee14ade783c6f6617019f1
Komal Thareja <kthare10@renci.org>
2023-10-30 18:39:07 +0000

    remove un-needed config

--- inference/pycbc/gw_output/run.sh
+++ inference/pycbc/gw_output/run.sh
@@ -6,6 +6,6 @@ cd /home/poseidon/workflows/pycbc/inference/pycbc/data/
 cd /home/poseidon/workflows/pycbc/inference/pycbc/gw_output/
 pegasus-plan --conf ./pegasus-properties.conf --dir submitdir --output-sites local --sites condorpool_copy,local --cluster label,horizontal --cleanup inplace --relative-dir work -q --submit --dax gw.dax
 sleep 60
-while [[ "$(pegasus-status | head -n 2 | tail -n 1)" != "(No matching jobs found in Condor Q)" ]]; do
+while [[ "$(pegasus-status | head -n 2 | tail -n 1)" != "(no matching jobs found in Condor Q)" ]]; do
    sleep 30
 done
