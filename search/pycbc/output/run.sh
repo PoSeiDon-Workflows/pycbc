121d86a2ab30e11dc8c5ec4bb098a4986e8ff6db
Komal Thareja <kthare10@renci.org>
2024-01-05 14:09:06 -0500

    update run.sh

--- search/pycbc/output/run.sh
+++ search/pycbc/output/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cd /home/poseidon/workflows/pycbc/inference/pycbc/data/
-./get.sh
+cd /home/poseidon/workflows/pycbc/search/pycbc/data/
+./get.sh .
+./bank.sh .
+./stats.sh .
 
-cd /home/poseidon/workflows/pycbc/inference/pycbc/gw_output/
+cd /home/poseidon/workflows/pycbc/search/pycbc/output/
 pegasus-plan --conf ./pegasus-properties.conf --dir submitdir --output-sites local --sites condorpool_copy,local --cluster label,horizontal --cleanup inplace --relative-dir work -q --submit --dax gw.dax
 sleep 60
 while [[ "$(pegasus-status | head -n 2 | tail -n 1)" != "(no matching jobs found in Condor Q)" ]]; do
