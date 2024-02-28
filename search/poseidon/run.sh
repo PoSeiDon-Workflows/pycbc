cf394da83cfe6651d20640f34ae8f7d8d5ca32ae
Rajiv Mayani <mayani@isi.edu>
2024-02-28 11:59:27 -0800

    feat: initial commit of pycbc search workflow for poseidon

---
+++ search/poseidon/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DIR=dags
+
+pegasus-graphviz --label=xform \
+                 --output search.png \
+                 workflow.yml &> /dev/null
+
+pegasus-plan -vvvvvv \
+             --conf pegasus.properties \
+             --cluster label,horizontal \
+             --sites condorpool_copy \
+             --output-sites local \
+             --dir ${DIR} \
+             --relative-submit-dir . \
+             --cleanup leaf \
+             --force \
+             --submit \
+             workflow.yml
+
+# Skip downloading pycbc.tar and importing it via docker over and over again.
+# Replace pycbc.tar with empty string
+# Replace docker_init with container_init and set cont_image to pycbc
+# find ${DIR} -name '*.sh' -exec sed -i '' -e 's/pycbc.tar//' -e 's/^docker_init pycbc/container_init ; cont_image='pycbc'/g' {} \;
